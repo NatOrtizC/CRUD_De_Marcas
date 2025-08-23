54f506228e53fabb58e2f418ebee0fd1df326b43
Laura Nataly Ortiz Cruz <natyorticruz@gmail.com>
2025-08-23 15:43:01 -0500

    update the serialization number automatically

--- project/serializers.py
+++ project/serializers.py
@@ -1,8 +1,16 @@
+from uuid import uuid4
 from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Project
         fields = ('id', 'brand', 'holder', 'registration_number', 'country', 'status', 'createdAt', 'updateAt')
-        read_only_fields = ('createdAt',)
+        read_only_fields = ('createdAt', 'updateAt', 'registration_number')
+        
+        
+    def create(self, validated_data):
+        if not validated_data.get('registration_number'):
+            validated_data['registration_number'] = f'REG-{uuid4().hex[:8].upper()}'
+        return super().create(validated_data)
